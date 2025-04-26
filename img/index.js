161e355298b98de32b1a3a8d76197c6b8770f18a
AakashAkku1 <164624759+AakashAkku1@users.noreply.github.com>
2025-04-26 20:05:41 +0530

    Add files via upload

---
+++ img/index.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#element', {
+    strings: ['Happy Birthday my Love', 'Thank You for coming in my Life ','Thank You for giving me Happines','Love You Endless my Sweetheart'] ,
+    typeSpeed: 50,
+    backSpeed:50,
+    loop: true,
+});
